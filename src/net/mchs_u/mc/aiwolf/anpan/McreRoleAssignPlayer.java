522582f8a857cd1b9df36b073dd4fa3f3667a7e9
mcre <mc@mchs-u.net>
2016-05-29 17:51:05 +0900

    最初のComit

---
+++ src/net/mchs_u/mc/aiwolf/anpan/McreRoleAssignPlayer.java
@@ -0,0 +1,16 @@
+package net.mchs_u.mc.aiwolf.anpan;
+
+import org.aiwolf.client.base.player.AbstractRoleAssignPlayer;
+
+public class McreRoleAssignPlayer extends AbstractRoleAssignPlayer {
+
+	public McreRoleAssignPlayer() {
+		setWerewolfPlayer(new McreWerewolf());
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+}
